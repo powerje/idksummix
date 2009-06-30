1e2e2a03f8882732badfd59e770dbd178e555ee6
heathen79 <heathen79@5345928c-6058-11de-8d08-39e76c6f0fc3>
2009-06-30 15:45:07 +0000

    Added Utility class and getBits(x, p, n) (get n bits from position p out of x)

--- src/summixSimulator/SummiX_Utilities.java
+++ src/summixSimulator/SummiX_Utilities.java
@@ -2,10 +2,6 @@ package summixSimulator;
 
 public class SummiX_Utilities {
 	public static short getBits(short data, int p, int n) {
-		short bitmask = (short) ((short) ~0 << (16 - n));
-		int i =  ((int)bitmask >>> n);
-		System.out.println(Integer.toHexString((int)bitmask).substring(4,8));
-		System.out.println(Integer.toHexString(i));
-		return (short) ((data >>> (16-p)) & ((1 << n)-1));
+		return (short) ((data >>> (16-p-n)) & ((1 << n)-1));
 	}
 }
