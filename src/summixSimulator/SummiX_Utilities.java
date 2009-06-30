1f59c2d82cccb87f418a3074907caef87ab482e3
Jazzepi <jazzepi@users.noreply.github.com>
2009-06-30 17:20:05 +0000

    Small update to utility class to add public enum

--- src/summixSimulator/SummiX_Utilities.java
+++ src/summixSimulator/SummiX_Utilities.java
@@ -4,4 +4,27 @@ public class SummiX_Utilities {
 	public static short getBits(short data, int p, int n) {
 		return (short) ((data >>> (16-p-n)) & ((1 << n)-1));
 	}
+	
+	public enum InstructionCode{
+		ADD,
+		ADD2,
+		AND,
+		AND2,
+		BRN,
+		BRZ,
+		BRP,
+		DBUG,
+		JSR,
+		JSRR,
+		LD,
+		LDI,
+		LDR,
+		LEA,
+		NOT,
+		RET,
+		ST,
+		STI,
+		STR,
+		TRAP
+	}
 }
