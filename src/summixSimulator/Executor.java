7fd59293d08baa335bf8c052b868c3e1c1d4cdd5
dlstottlemire <dlstottlemire@5345928c-6058-11de-8d08-39e76c6f0fc3>
2009-07-01 23:58:08 +0000

    Completed Add and And operations

--- src/summixSimulator/Executor.java
+++ src/summixSimulator/Executor.java
@@ -4,33 +4,51 @@ import summixSimulator.SummiX_Utilities.InstructionCode;
 
 public class Executor {
 	public Executor(SummiX_Machine machine, short data, InstructionCode op) {
+		short sr1, sr2, dr, imm5, pc, pgoffset9, nzp;
 		switch (op) {
 			case ADD:
-				short sr1 = SummiX_Utilities.getBits(data, 7, 3);
-				short sr2 = SummiX_Utilities.getBits(data, 12, 3);
-				short dr  = SummiX_Utilities.getBits(data, 4, 3);
-				machine.setRegister(dr, (short) (sr1 + sr2));
+				sr1 = SummiX_Utilities.getBits(data, 7, 3);
+				sr2 = SummiX_Utilities.getBits(data, 12, 3);
+				dr  = SummiX_Utilities.getBits(data, 4, 3);
+				machine.setRegister(dr, (short) (machine.loadRegister(sr1) 
+												+ machine.loadRegister(sr2)));
 				break;
 			case ADD2:
+				sr1 = SummiX_Utilities.getBits(data, 7, 3);
+				imm5 = SummiX_Utilities.getBits(data, 11, 5);
+				dr  = SummiX_Utilities.getBits(data, 4, 3);
+				machine.setRegister(dr, (short) (machine.loadRegister(sr1) 
+												+ imm5));
 				break;
 			case AND:
+				sr1 = SummiX_Utilities.getBits(data, 7, 3);
+				sr2 = SummiX_Utilities.getBits(data, 12, 3);
+				dr  = SummiX_Utilities.getBits(data, 4, 3);
+				machine.setRegister(dr, (short) (machine.loadRegister(sr1) 
+												& machine.loadRegister(sr2)));
 				break;
 			case AND2:
+				sr1 = SummiX_Utilities.getBits(data, 7, 3);
+				imm5 = SummiX_Utilities.getBits(data, 11, 5);
+				dr  = SummiX_Utilities.getBits(data, 4, 3);
+				machine.setRegister(dr, (short) (machine.loadRegister(sr1) 
+												& imm5));
 				break;
-			case BRN:
+			case BRX:
+				pc = machine.getPC();
+				pgoffset9 = SummiX_Utilities.getBits(data, 7, 9);
+				nzp = SummiX_Utilities.getBits(data, 4, 3);
+				
+				if (nzp == 1){  // n bit
+					
+				}
+				else if (nzp == 2){  // z bit
+					
+				}
+				else if (nzp == 4){  // p bit
+					
+				}
 				break;
-			case BRZ:
-				break;
-			case BRP:
-				break;
-			case BRA:
-				break;
-			case BRGE:
-				break;
-			case BRLE:
-				break;
-			case NOP:
-			break;
 			case DBUG:
 				break;
 			case JSR:
