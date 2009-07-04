4c584ec41d0c18b1dbf848c8a8f7435889094608
James Power <heathen79@gmail.com>
2009-07-04 17:43:02 +0000

    Wrote shortToHexString in Utilities used to print hexStrings prettily (instead of manually checking to see if it got sign extended when casted to an int each time we turn shorts into strings)

--- src/summixSimulator/SummiX_Utilities.java
+++ src/summixSimulator/SummiX_Utilities.java
@@ -9,6 +9,15 @@ public class SummiX_Utilities {
 		short s = (short) ((data >>> (16-p-n)) & ((1 << n)-1));
 		return (short) (s << (16-p-n));
 	}
+	
+	public static String shortToHexString(short data) {
+		String returnVal = Integer.toHexString((int) data);
+		if (returnVal.length() > 4) {
+			returnVal = returnVal.substring(returnVal.length() - 4, returnVal.length());
+		}
+		return "0x" + returnVal;
+	}
+	
 	public enum InstructionCode{
 		ADD,
 		ADD2,
