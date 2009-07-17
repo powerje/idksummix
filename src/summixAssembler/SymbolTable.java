b17e4a05dc3597b8f46b69a54f1c39a5d363a2cb
James Power <heathen79@gmail.com>
2009-07-17 17:01:43 +0000

    put symbols/literals back in the table

--- src/summixAssembler/SymbolTable.java
+++ src/summixAssembler/SymbolTable.java
@@ -1,5 +1,24 @@
 package summixAssembler;
 
 public class SymbolTable {
-
+	public class Symbol {
+		private String name;
+		private int value;
+		
+		public String getName() {
+			return name;
+		}
+		
+		public int getValue() {
+			return value;
+		}
+		
+		public void setName(String name) {
+			this.name = name;
+		}
+		
+		public void setValue(int value) {
+			this.value = value;
+		}
+	}
 }
