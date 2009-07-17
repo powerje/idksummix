82a45a162df8a28be6a06407f38ea10f7bb6ba0c
James Power <heathen79@gmail.com>
2009-07-17 15:27:09 +0000

    Seperated out the Literal and Symbol class as I couldn't get them to nicely sit inside LiterlTable and SymbolTable classes

---
+++ src/summixAssembler/Symbol.java
@@ -0,0 +1,22 @@
+package summixAssembler;
+
+public class Symbol {
+	private String name;
+	private int value;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public int getValue() {
+		return value;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setValue(int value) {
+		this.value = value;
+	}
+}
