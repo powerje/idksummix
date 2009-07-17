a0e1a5a016ae2b3900ed2a31f995693564fa36ba
James Power <heathen79@gmail.com>
2009-07-17 15:27:29 +0000

    and added main to this thing

--- src/summixAssembler/Assembler.java
+++ src/summixAssembler/Assembler.java
@@ -4,6 +4,6 @@ import java.io.IOException;
 
 public class Assembler {
 	public static void main(String[] args) throws IOException {
-		LiteralTable.Literal l = new (LiteralTable.Literal());
+	
 	}
 }
