a1946ec6e8cf74c7d7051d29aa108fe76cd94b6a
James Power <heathen79@gmail.com>
2009-07-17 22:38:53 +0000

    Got rid of getters and setters for literals and symbols... they're stupid, just made name/value/address public, makes more sense I believe... if you disagree read this: http://www.javaworld.com/javaworld/jw-09-2003/jw-0905-toolbox.html?page=1
    I don't know if that article really agrees with me but it's really long so by the time you're done you'll forget what you read it for

--- src/summixAssembler/LiteralTable.java
+++ src/summixAssembler/LiteralTable.java
@@ -4,25 +4,9 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class LiteralTable {
-	public class Literal {
-		private String name;
-		private int address;
-		
-		public String getName() {
-			return name;
-		}
-		
-		public int getAddress() {
-			return address;
-		}
-		
-		public void setName(String name) {
-			this.name = name;
-		}
-		
-		public void setAddress(int address) {
-			this.address = address;
-		}
+	private class Literal {
+		public String name;
+		public int address;
 	}
 	
 	private static Set<Literal> literals = new HashSet<Literal>(5);
--- src/summixAssembler/SymbolTable.java
+++ src/summixAssembler/SymbolTable.java
@@ -4,25 +4,9 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class SymbolTable {
-	public class Symbol {
-		private String name;
-		private int value;
-		
-		public String getName() {
-			return name;
-		}
-		
-		public int getValue() {
-			return value;
-		}
-		
-		public void setName(String name) {
-			this.name = name;
-		}
-		
-		public void setValue(int value) {
-			this.value = value;
-		}
+	private class Symbol {
+		public String name;
+		public int value;
 	}
 	
 	private static Set<Symbol> symbols = new HashSet<Symbol>(5);
