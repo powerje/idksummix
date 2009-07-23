a4f1b172131c394c27ed292548b1f9d9d9152990
dlstottlemire <dlstottlemire@5345928c-6058-11de-8d08-39e76c6f0fc3>
2009-07-23 19:28:11 +0000

    ?

--- src/summixAssembler/LocationCounter.java
+++ src/summixAssembler/LocationCounter.java
@@ -1,5 +1,22 @@
 package summixAssembler;
 
 public class LocationCounter {
+	
+	private static short addr = 0;
+	
+	public void incrementAmt(short amount) {
+		
+		addr = (short) (addr + amount);
+	}
+	
+	public void set(short address){
+	
+		addr = address;
+	}
+	
+	public short getAddress(){
+		
+		return addr;
+	}
 
 }
