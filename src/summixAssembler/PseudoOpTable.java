c14a967b6bb1617e72bebb2c34367bd91a6438ea
heathen79 <heathen79@5345928c-6058-11de-8d08-39e76c6f0fc3>
2009-07-24 18:13:50 +0000

    Added a getShiftedOp to pseudoOpTable, maybe we'll want both?

--- src/summixAssembler/PseudoOpTable.java
+++ src/summixAssembler/PseudoOpTable.java
@@ -20,6 +20,15 @@ public class PseudoOpTable {
 		}
 		return returnVal;
 	}
+	
+	public short getShiftedOp(String name){
+		//we want this bitshifted or not?
+		short returnVal = 0;
+		if (pseudoOps.containsKey(name)) {
+			returnVal =  (short) (pseudoOps.get(name).op << 12);
+		}
+		return returnVal;
+	}
 
 	public short getSize(String name){
 		short returnVal = 0;
