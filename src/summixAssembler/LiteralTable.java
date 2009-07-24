f9fb4d3f788558a89780a64b2ae6d7a4e7074eac
heathen79 <heathen79@5345928c-6058-11de-8d08-39e76c6f0fc3>
2009-07-24 18:52:10 +0000

    SymbolTable is just about done... I had to move the Symbol class out of it because it wanted an instantiation of a SymbolTable object before it would let me make a new Symbol item... stupid...

--- src/summixAssembler/LiteralTable.java
+++ src/summixAssembler/LiteralTable.java
@@ -6,7 +6,7 @@ import java.util.Map;
 import java.util.Set;
 
 public class LiteralTable {
-	Map<Short, Short> literals = new HashMap<Short, Short>();
+	private static Map<Short, Short> literals = new HashMap<Short, Short>();
 	
 	public void input(short key, short addr) {
 		if (!literals.containsKey(key)) {
