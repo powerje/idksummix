c3b55f9c18889c3c7324f3b961ac676679a3b04e
heathen79 <heathen79@5345928c-6058-11de-8d08-39e76c6f0fc3>
2009-07-24 18:12:42 +0000

    Edited pseudoOps a little bit to get rid of the error message it was giving, because it vexes me, even though it means nothing... maybe I should finish the pseudoOpTable as well
    Added overloaded parameters to the LiteralTable

--- src/summixAssembler/PseudoOpTable.java
+++ src/summixAssembler/PseudoOpTable.java
@@ -4,15 +4,30 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class PseudoOpTable {
-	Map<String, Short> pseudoOps = new HashMap<String, Short>();
+	private class PseudoOp {
+		public short op;
+		public short size;
+	}
+	
+	Map<String, PseudoOp> pseudoOps = new HashMap<String, PseudoOp>();
 	
 
-public short getOp(String name){
-	
-}
+	public short getOp(String name){
+		//we want this bitshifted or not?
+		short returnVal = 0;
+		if (pseudoOps.containsKey(name)) {
+			returnVal = pseudoOps.get(name).op;
+		}
+		return returnVal;
+	}
 
-public short getSize(String name){
-	
-}
+	public short getSize(String name){
+		short returnVal = 0;
+		if (pseudoOps.containsKey(name)) {
+			returnVal = pseudoOps.get(name).size;
+		}
+		return returnVal;
+		
+	}
 
 }
