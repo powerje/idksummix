9e925fb5c989b01e383da540e6ac868e2723bdc5
heathen79 <heathen79@5345928c-6058-11de-8d08-39e76c6f0fc3>
2009-07-25 01:02:48 +0000

    I got rid of the MachineOp and PseudoOp classes... I was a bonehead and made some methods static when all that needs to be static are the maps, now  it works fine as a helper class within the other

--- src/summixAssembler/MachineOpTable.java
+++ src/summixAssembler/MachineOpTable.java
@@ -4,42 +4,38 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class MachineOpTable {
-	
+	public class MachineOp {
+		public short op;			//4 bit op code
+		public short size;			//size of the op code
+		
+		MachineOp(int op, int size) {
+			this.op = (short) op;
+			this.size = (short) size;
+		}
+		
+		public String toString() {
+			return "(" + this.op + ", " + this.size + ")";
+		}
+		
+	}
+
 	private static Map<String, MachineOp> machineOps = new HashMap<String, MachineOp>();
+	
+	public void initialize() {
+		machineOps.put("ADD", new MachineOp(0x0001, 1));
+	}
 	
 	public short getOp(String name){
 		//we want this bitshifted or not?
 		short returnVal = 0;
 		if (machineOps.containsKey(name)) {
-			returnVal = machineOps.get(name).op;
-		} else {
-			//error
-		}
-		return returnVal;
-	}
-	
-	public short getShiftedOp(String name){
-		//we want this bitshifted or not?
-		short returnVal = 0;
-		if (machineOps.containsKey(name)) {
-			returnVal =  (short) (machineOps.get(name).op << 12);
+			returnVal = (short) (machineOps.get(name).op << 12);
 		} else {
 			//error
 		}
 		return returnVal;
 	}
 
-	public boolean isVariable(String name) {
-		boolean returnVal = false;
-		
-		if (machineOps.containsKey(name)) {
-			returnVal = machineOps.get(name).isVariable;
-		} else {
-			//error
-		}
-		return returnVal;
-	}
-	
 	public short getSize(String name){
 		short returnVal = 0;
 		if (machineOps.containsKey(name)) {
