0babfbf3b32b9016201353d3a925a456b031fc6d
Jazzepi <jazzepi@users.noreply.github.com>
2009-07-26 04:02:23 +0000

    Added the last of the skeleton to the constructor, so that it's "complete" logically. All we need to do now is fill in the underlying classes.

--- src/summixAssembler/Assembler.java
+++ src/summixAssembler/Assembler.java
@@ -24,8 +24,13 @@ public class Assembler {
 		boolean fileSwitch = false;
 		boolean verboseSwitch = false;
 		boolean goodBuild = false;
+		boolean objectPrinted = false;
+		boolean listPrinted = false;
 		String sourceFileName = "";
 		TextFile sFile = null;
+		TextFile p1File = null;
+		TextFile p2File = null;
+		TextFile lFile = new TextFile();
 		int i = 0;
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -86,21 +91,88 @@ public class Assembler {
 				System.out.println(e);
 			}
 		}
-
 		//Give Pass1 the sFile as a parameter, and then take the return value as p1File
+		Pass1 pass1 = new Pass1(sFile);
+		p1File = pass1.processFile();
 		
 		//If -d switch is set, dump p1File, symbol table, and literal table to the display with the Dump() helper function
+		if (dumpSwitch)
+		{
+			System.out.println("Diagonistic dump mode has been enabled. Displaying intermediate file, symbol table, and literal table.");
+			System.out.println("");
+			p1File.display();
+			System.out.println("");
+			SymbolTable.display();
+			System.out.println("");
+			LiteralTable.display();
+			System.out.println("");
+		}
 
 		//Give Pass2 the p1File and then take the return value as p2File
-
+		Pass2 pass2 = new Pass2(p1File);
+		p2File = pass2.processFile();
+		
 		//Create lFile from sFile (or maybe p1File?), p2File, symbol table, and literal table
-		//Use the overloaded constructor of the TextFile to do this
-		//Code should look something like this TextFile lFile = new TextFile(sFile, p2File);
-
+		lFile = makelFile(sFile, p2File);
+		
 		//If -v switch is set, display p2File and lFile
+		if (verboseSwitch)
+		{
+			System.out.println("Verbose mode has been enabled. Displaying list file, and object file.");
+			System.out.println("");
+			lFile.display();
+			System.out.println("");
+			p2File.display();
+			System.out.println("");
+		}
+		
+		//Write lFile and p2File to files with extensions .l and .o respectively. .l is for list and .o is for object
+		try {
+			lFile.write(sourceFileName.concat(".l"));
+			listPrinted = true;
+		}
+		catch(IOException e)
+		{
+			System.out.println(e);
+			
+		}
+	
+		try {
+			p2File.write(sourceFileName.concat(".o"));
+			objectPrinted = true;
+		}
+		catch(IOException e)
+		{
+			System.out.println(e);
+		}
 
-		//Write lFile and p2File to files with some sort of extension. Probably .l and .o respectively. .l is for list and .o is for object
-
+		
 		//Display success message for names of files written, and maybe some info about them like size
+		if (listPrinted)
+		{
+			System.out.println(sourceFileName.concat(".l") + " was written successfully.");
+		}
+		else
+		{
+			System.out.println(sourceFileName.concat(".l") + " was not written successfully.");	
+		}
+		
+		if (objectPrinted)
+		{
+			System.out.println(sourceFileName.concat(".o") + " was written successfully.");
+		}
+		else
+		{
+			System.out.println(sourceFileName.concat(".o") + " was not written successfully.");	
+		}
+		br.close();
+	}
+
+	
+	private static TextFile makelFile(TextFile sFile, TextFile p2File)
+	{		
+		TextFile lFile = null;
+		
+		return lFile;
 	}
 }
