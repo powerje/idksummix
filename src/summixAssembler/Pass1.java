8a22988aa25dade955d3abd95a458a5e16c1aabe
Jazzepi <jazzepi@users.noreply.github.com>
2009-07-26 03:10:45 +0000

    added some stuff to pass1 and pass2 to make the complier happy. Pass1 and two need methods to process the file

--- src/summixAssembler/Pass1.java
+++ src/summixAssembler/Pass1.java
@@ -2,4 +2,17 @@ package summixAssembler;
 
 public class Pass1 {
 
+	TextFile body;
+	
+	public Pass1(TextFile incomingSource)
+	{
+		body = incomingSource;
+	}
+	
+	public TextFile processFile()
+	{
+		
+		//This is where all the work has to be done. This is just temp code to make the complier happy for the assembler main() class
+		return body;
+	}
 }
--- src/summixAssembler/Pass2.java
+++ src/summixAssembler/Pass2.java
@@ -1,5 +1,16 @@
 package summixAssembler;
 
 public class Pass2 {
-
+	TextFile body;
+	
+	public Pass2(TextFile incomingSource)
+	{
+		body = incomingSource;
+	}
+	
+	public TextFile processFile()
+	{
+		//This is where all the work has to be done. This is just temp code to make the complier happy for the assembler main() class
+		return body;
+	}
 }
