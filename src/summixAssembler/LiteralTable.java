7c0f582fb62884e727f26b150dca89696a6e7d22
James Power <heathen79@gmail.com>
2009-07-26 15:17:14 +0000

    added javadoc to these classes

--- src/summixAssembler/LiteralTable.java
+++ src/summixAssembler/LiteralTable.java
@@ -3,9 +3,19 @@ package summixAssembler;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * 
+ * @author Jim
+ *
+ */
 public class LiteralTable {
 	private static Map<Short, Short> literals = new HashMap<Short, Short>();
-	
+
+	/**
+	 * 
+	 * @param key the key to add
+	 * @param addr the address to give the new literal
+	 */
 	public static void input(short key, short addr) {
 		if (!literals.containsKey(key)) {
 			literals.put(key, addr);
@@ -14,18 +24,38 @@ public class LiteralTable {
 		}
 	}
 	
+	/**
+	 * 
+	 * @param key the key to add
+	 * @param addr the address to give the new literal
+	 */
 	public static void input (String key, String addr) {
 		input(Short.parseShort(key),Short.parseShort(addr));
 	}
-	
+
+	/**
+	 * 
+	 * @param key the key to add
+	 * @param addr the address to give the new literal
+	 */
 	public static void input (short key, String addr) {
 		input(key,Short.parseShort(addr));
 	}
-	
+
+	/**
+	 * 
+	 * @param key the key to add
+	 * @param addr the address to give the new literal
+	 */	
 	public static void input (String key, short addr) {
 		input(Short.parseShort(key),addr);
 	}
 	
+	/**
+	 * 
+	 * @param key the key to search for
+	 * @return the address of the literal given by key
+	 */
 	public static short getAddress(short key) {
 		short returnVal = 0;
 		if (literals.containsKey(key)) {
@@ -36,6 +66,9 @@ public class LiteralTable {
 		return returnVal;
 	}
 	
+	/**
+	 * Display the contents of the literal table to the console.
+	 */
 	public static void display() {
 		System.out.println( "Literal Table:\n" +
 						 	"Format: key (address)");
--- src/summixAssembler/PseudoOpTable.java
+++ src/summixAssembler/PseudoOpTable.java
@@ -3,24 +3,45 @@ package summixAssembler;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * 
+ * @author Jim
+ *
+ */
 public class PseudoOpTable {
 
+	/**
+	 * 
+	 * @author Jim
+	 *
+	 */
 	private static class PseudoOp {
 		public short size;			//size of the op code
-		public boolean isVariable;	//is the size dependant on the instruction code?
+		public boolean isVariable;	//is the size dependent on the instruction code?
 		
+		/**
+		 * 
+		 * @param size the size of the pseudo op to be created
+		 * @param isVariable whether or not the size of the pseudo op to be created is variable
+		 */
 		PseudoOp(int size, boolean isVariable) {
 			this.size = (short) size;
 			this.isVariable = isVariable;
 		}
 		
+		/**
+		 * Returns a String object representing the specified pseudo op. 
+		 */
 		public String toString() {
 			return "(" + this.size + ", " + this.isVariable + ")";
 		}
 	}
 	
 	private static Map<String, PseudoOp> pseudoOps = new HashMap<String, PseudoOp>();
-	
+
+	/**
+	 * Initializes the pseudo op table.
+	 */
 	public static void initialize() {
 		//format:	Mnemonic, size, relativity
 		pseudoOps.put(".ORIG", new PseudoOp(0, false));
@@ -31,6 +52,11 @@ public class PseudoOpTable {
 		pseudoOps.put(".BLKW", new PseudoOp(0, true));
 	}
 	
+	/**
+	 * 
+	 * @param name the key to search for the pseudo op
+	 * @return whether or not the pseudo op is of variable size
+	 */
 	public static boolean isVariable(String name) {
 		boolean returnVal = false;
 		
@@ -42,15 +68,25 @@ public class PseudoOpTable {
 		return returnVal;
 	}
 	
+	/**
+	 * 
+	 * @param name the key to search for the pseudo op
+	 * @return the size of the pseudo op
+	 */
 	public static short getSize(String name){
 		short returnVal = 0;
 		if (pseudoOps.containsKey(name)) {
 			returnVal = pseudoOps.get(name).size;
+		} else {
+			//error
 		}
 		return returnVal;
 		
 	}
 
+	/**
+	 * Display the pseudo op table to the console.
+	 */
 	public static void display() {
 		System.out.println( "Pseudo Op Table:\n" +
 						 	"Format: Mnemonic (size, variable size)");
