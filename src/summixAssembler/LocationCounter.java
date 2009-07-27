0ae3c4087d1412fb64159d896e3df090f257a88b
Dlstottlemire <Dlstottlemire@5345928c-6058-11de-8d08-39e76c6f0fc3>
2009-07-27 16:40:22 +0000

    ...

--- src/summixAssembler/LocationCounter.java
+++ src/summixAssembler/LocationCounter.java
@@ -2,23 +2,12 @@ package summixAssembler;
 
 public class LocationCounter {
 	
-	private static short curAddr = 0;
+	private static int curAddr = 0;
 	
 	public static void incrementAmt(short amount) {
-		
-		short offset = (short) ((curAddr << 7) >>> 9);
-		//short curPage = (short) ((curAddr >> 9) << 7);
-		//short newPage = (short) (((curAddr + amount) >> 9) << 7);
+			curAddr = ((short)curAddr + amount);
+		}
 
-		if (((offset + amount)) >= 511){
-			//System.out.print("Error:Location Counter Page Rollover! Previous Page: "+ curPage + "New Page: "+ newPage);
-			//curAddr = (short) (curAddr + amount);
-		}
-		else{
-			curAddr = (short) (curAddr + amount);
-		}
-	}
-	
 	public static void set(short address){
 	
 		curAddr = address;
@@ -26,7 +15,7 @@ public class LocationCounter {
 	
 	public static short getAddress(){
 		
-		return curAddr;
+		return (short) curAddr;
 	}
 }
 
