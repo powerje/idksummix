320d2d1ace585ac7e1946d4cff0190044759d3c5
Jazzepi <Jazzepi@5345928c-6058-11de-8d08-39e76c6f0fc3>
2009-07-31 23:13:15 +0000

    Added token type which is the enum used in the token class. TextFile's returnToken() class now returns a Token which contains methods to access the string the token is composed of, and a method to access it's enum.

--- src/summixAssembler/TextFile.java
+++ src/summixAssembler/TextFile.java
@@ -12,107 +12,106 @@ import java.util.StringTokenizer;
 
 
 public class TextFile {
-	
-	
+
+
 	/** The main storage body for each string that represents the slurped file*/
 	private ArrayList<String> body;
 	/** The horizontal depth into a string of the body array*/ 
 	private int posPointer;
 	/** The vertical depth down the rows of strings in the body array*/
 	private int rowPointer;
-	
+
 	/**
 	 * Constructor for TextFile
 	 * 
 	 * @param inputFilename Name of the file to be slurped
 	 * @throws IOException Missing file
 	 */
-	
+
 	public TextFile(String inputFilename) throws IOException
 	{
 		body = new ArrayList<String>();
 		posPointer = 0;
 		rowPointer = 0;
-		
+
 		BufferedReader in = new BufferedReader(new FileReader(inputFilename));
-		
+
 		while (in.ready()) //While not end of file, take in another line
 		{
 			String currentInput = in.readLine();
 			body.add(currentInput);
 		}
-		
+
 		in.close();
 	}
-	
+
 	public TextFile()
 	{
 		body = new ArrayList<String>();
 		posPointer = 0;
 		rowPointer = 0;
 	}
-	
-	public String getToken()
+
+	public Token getToken()
 	{
 		String rVal = body.get(rowPointer); //Get the string out of the array located at the rowPointer
 		rVal = rVal.substring(posPointer); //Get the portion of the string to the right of the posPointer
 		int numOfSpaces = 0;
 		boolean haveGottenToken = false, insideQuotes = false, insideComment = false;
 		String temp, returnTok = "";
-		
+
 		StringTokenizer st = new StringTokenizer(rVal,"; \t\"",true);
-	    
+
 		while (!haveGottenToken && st.hasMoreTokens()) //Until a well formed token is ready, or we run out of tokens, keep taking them out
 		{
-	    	temp = st.nextToken();
-	    	
-	    	if (insideComment) //If inside of a comment, just add everything till I get to the end
-	    	{
-	    		returnTok = returnTok.concat(temp);
-	    	}
-	    	else //I'm not inside a comment, then we want to be careful
-	    	{
-		    	if (insideQuotes && !temp.startsWith("\"")) //inside quote, but haven't found the ending quotes
-		    	{
-		    		returnTok = returnTok.concat(temp);
-		    	}
-		    	else if (insideQuotes && temp.startsWith("\"")) //inside quote, but just found the ending quotes, token done
-		    	{
-		    		returnTok = returnTok.concat(temp);
-		    		insideQuotes = false;
-		    		haveGottenToken = true;
-		    	}
-		    	else //I'm not inside of a comment, and I'm not inside of a quote
-		    	{
-		    		switch (temp.charAt(0)) //Make decision based on the first element
-		    		{
-		    			case '"': //Beginning of a set of quotation set
-		    				insideQuotes = true;
-		    				returnTok = returnTok.concat(temp);
-		    				break;
-		    			case ';': //Beginning a comment
-		    				insideComment = true;
-		    				returnTok = returnTok.concat(temp);
-		    				break;
-		    			case ' ': //Useless space, count for pos increment
-		    				numOfSpaces++;
-		    				break;
-		    			case '\t': //Useless tab, count for pos increment
-		    				numOfSpaces++;
-		    				break;
-		    			default: //Just an alphanumeric string, we want the whole thing
-		    				haveGottenToken = true;
-		    				returnTok = returnTok.concat(temp);
-		    		}
-		    	}
-	    	}
+			temp = st.nextToken();
 
-//	    	System.out.println("Token extracted:" + temp); //Debugging code, REMOVEME
-//	    	System.out.println("Token built:" + returnTok); //Debugging code, REMOVEME
-	    	}
-		
-//		System.out.print("Token to be returned:" + returnTok); //Debugging code, REMOVEME
-		
+			if (insideComment) //If inside of a comment, just add everything till I get to the end
+			{
+				returnTok = returnTok.concat(temp);
+			}
+			else //I'm not inside a comment, then we want to be careful
+			{
+				if (insideQuotes && !temp.startsWith("\"")) //inside quote, but haven't found the ending quotes
+				{
+					returnTok = returnTok.concat(temp);
+				}
+				else if (insideQuotes && temp.startsWith("\"")) //inside quote, but just found the ending quotes, token done
+				{
+					returnTok = returnTok.concat(temp);
+					insideQuotes = false;
+					haveGottenToken = true;
+				}
+				else //I'm not inside of a comment, and I'm not inside of a quote
+				{
+					switch (temp.charAt(0)) //Make decision based on the first element
+					{
+					case '"': //Beginning of a set of quotation set
+						insideQuotes = true;
+						returnTok = returnTok.concat(temp);
+						break;
+					case ';': //Beginning a comment
+						insideComment = true;
+						returnTok = returnTok.concat(temp);
+						break;
+					case ' ': //Useless space, count for pos increment
+						numOfSpaces++;
+						break;
+					case '\t': //Useless tab, count for pos increment
+						numOfSpaces++;
+						break;
+					default: //Just an alphanumeric string, we want the whole thing
+						haveGottenToken = true;
+						returnTok = returnTok.concat(temp);
+					}
+				}
+			}
+
+
+
+		}
+
+
 		if (body.get(rowPointer).substring(posPointer).isEmpty()) //If at end of a line, go down to the next line with the pos pointer
 		{
 			rowPointer++;
@@ -122,26 +121,51 @@ public class TextFile {
 		{
 			posPointer += returnTok.length() + numOfSpaces;
 		}
-		
-		return returnTok;
+
+		TokenType tempType;
+
+		//Investigate the generated string to see which type of token it is
+		if(returnTok.isEmpty())
+		{
+			tempType = TokenType.EOL;
+		}
+		else if(returnTok.startsWith("\"") && returnTok.endsWith("\""))
+		{
+			tempType = TokenType.QUOTE;
+		}
+		else if(returnTok.startsWith("\"") && !returnTok.endsWith("\""))
+		{
+			tempType = TokenType.ERROR;
+		}
+		else if(returnTok.startsWith(";"))
+		{
+			tempType = TokenType.COMMENT;
+		}
+		else
+		{
+			tempType = TokenType.ALPHA;
+		}
+
+		return new Token(returnTok, tempType); 
+
 	}
-	
+
 	public String getLine()
 	{
 		String rVal = body.get(rowPointer); //Get the string out of the array located at the rowPointer
 		rVal = rVal.substring(posPointer); //Get the portion of the string to the right of the posPointer
-		
+
 		rowPointer++;
 		posPointer = 0;
-		
+
 		return rVal;
 	}
-	
-	
+
+
 	public void write(String outputFilename) throws IOException
 	{
 		int i = 0;
-		
+
 		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(outputFilename)));
 		while(i < body.size()) //Print out each line in the body representation of the text file
 		{
@@ -150,29 +174,29 @@ public class TextFile {
 		}
 		out.close();
 	}
-	
+
 	public void input(String entry)
 	{
 		body.add(entry);
 	}
-	
+
 	public void display()
 	{
 		int i = 0;
-		
+
 		while(i < body.size())
 		{
 			System.out.println(body.get(i));
 			i++;
 		}
 	}
-	
+
 	public void reset()
 	{
 		posPointer = 0;
 		rowPointer = 0;
 	}
-	
+
 	public boolean isEndOfFile()
 	{
 		if (body.size() == 0 || (body.size() == 1 && body.get(0).isEmpty())) //If file is empty, you're automatically at EoF
@@ -188,5 +212,5 @@ public class TextFile {
 			return false;
 		}
 	}
-	
+
 }
