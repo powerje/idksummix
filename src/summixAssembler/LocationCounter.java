4fc64fe3e1d8b1faa5286d91cf99771b28810200
Dlstottlemire <Dlstottlemire@5345928c-6058-11de-8d08-39e76c6f0fc3>
2009-08-03 18:29:25 +0000

    I am just laying out my logic for pass1

--- src/summixAssembler/LocationCounter.java
+++ src/summixAssembler/LocationCounter.java
@@ -1,17 +1,51 @@
 package summixAssembler;
 
+/**
+ * 
+ * @author Dan Stottlemire
+ *
+ */
+
 public class LocationCounter {
 	
-	private static int curAddr = 0;
+	static int curAddr;
 	
-	public static void incrementAmt(short amount) {
-			curAddr = ((short)curAddr + amount);
+	/**
+	 * Incrementer for the Location Counter
+	 * 
+	 * @param amount - The value to increment the Location Counter
+	 */
+	
+	public static void incrementAmt(int amount) {
+			curAddr = (short)(curAddr + amount);
 		}
-
-	public static void set(short address){
 	
+	// public static void incrementAfterLiteral(int amount){
+		
+	//} I do not think we need this.
+
+	/**
+	 * Setter for the Location Counter
+	 * 
+	 * @param address - The address which to set the Location Counter.
+	 * @param isRelative - A boolean which states if the Address is relative or not.
+	 */
+	
+	public static void set(int address, boolean isRelative){
+	
+		if (!isRelative){
 		curAddr = address;
+		}
+		else{
+		curAddr = 0;
+		}
 	}
+	
+	/**
+	 * Getter for the Location Counter
+	 * 
+	 * @return the current address of the Location Counter.
+	 */
 	
 	public static short getAddress(){
 		
