233af127b5870be7e0342bfa0525a83f1424dfc4
Dlstottlemire <Dlstottlemire@5345928c-6058-11de-8d08-39e76c6f0fc3>
2009-08-03 18:29:25 +0000

    I am just laying out my logic for pass1

--- src/summixAssembler/Pass1.java
+++ src/summixAssembler/Pass1.java
@@ -11,8 +11,38 @@ public class Pass1 {
 	
 	public TextFile processFile()
 	{
+		// processHeaderRecord
 		
-		//This is where all the work has to be done. This is just temp code to make the complier happy for the assembler main() class
+			// while (!incomingSource.isEndOfFile()){
+		
+			// processProgName
+		
+				// incomingSource.getToken(token)	
+		
+				// while (token.getType != ALPHA or QUOTE){
+		
+				// if token.getType == ERROR{
+					// error message
+				// }
+		
+				// else if (token.getType == COMMENT or EOL) and
+				// (!incomingSource.isEndOfFile()){
+					// incomingSource.getToken(token)
+					// }
+			// }
+		
+			// progName = ((token.getText()) + (n)spaces) 
+			// where n = 6 - (num_of_spaces(tokenText))
+			// }
+
+		
+			// getToken(token)
+		
+		
+		// set Location Counter
+		// store initial LC for later calculation of segment size
+		
+		
 		return body;
 	}
 }
