15ec3ed7d581d1ed804887b77681d82b28069e01
irwin.mikej <irwin.mikej@5345928c-6058-11de-8d08-39e76c6f0fc3>
2009-08-09 14:54:17 +0000

    added lfile

--- src/summixAssembler/ListFile.java
+++ src/summixAssembler/ListFile.java
@@ -88,6 +88,8 @@ public class ListFile {
 		String completeRow = null;
 		int progCount = 1;
 		
+		System.out.println("//////////////////////////////////////////STARTS HERE!!!!////////////////////////////");
+		
 		while(!source.isEndOfFile() && !p2.isEndOfFile())
 		{
 			sourceLine = ProcessLineSource(source);  //get first line
